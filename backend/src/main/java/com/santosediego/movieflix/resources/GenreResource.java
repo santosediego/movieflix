353cb1b4a548a50b9eda42829cad6604ac54bfb3
santosediego <diegoeustaquiodossantos@hotmail.com>
2021-02-26 06:08:37 -0300

    Generation of API documentation with Swagger

--- backend/src/main/java/com/santosediego/movieflix/resources/GenreResource.java
+++ backend/src/main/java/com/santosediego/movieflix/resources/GenreResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class GenreResource {
 	@Autowired
 	private GenreService service;
 
-	@RequestMapping
+	@GetMapping
 	public ResponseEntity<List<GenreDTO>> listAll(){
 		List<GenreDTO> listDTO = service.findAll();
 		return ResponseEntity.ok().body(listDTO);
