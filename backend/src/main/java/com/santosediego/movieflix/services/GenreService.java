3d9b7d778a49c268344b8e8d69e0f7bbdc596252
santosediego <diegoeustaquiodossantos@hotmail.com>
2021-02-27 12:45:40 -0300

    Fix: Add Transactional in GenreService

--- backend/src/main/java/com/santosediego/movieflix/services/GenreService.java
+++ backend/src/main/java/com/santosediego/movieflix/services/GenreService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.santosediego.movieflix.dto.GenreDTO;
 import com.santosediego.movieflix.entities.Genre;
@@ -15,7 +16,8 @@ public class GenreService {
 
 	@Autowired
 	private GenreRepository repository;
-	
+
+	@Transactional(readOnly = true)
 	public List<GenreDTO> findAll() {
 		List<Genre> list = repository.findAll();
 		List<GenreDTO> listDTO = list.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
