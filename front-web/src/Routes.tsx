a269761304af2a40fa8410f2de77bab5689108ac
santosediego <diegoeustaquiodossantos@hotmail.com>
2021-03-10 06:59:57 -0300

    Add Navbar

---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./core/components/Navbar";
+
+const Routes = () => (
+    <BrowserRouter>
+        <Navbar />
+    </BrowserRouter>
+);
+
+export default Routes;
---
+++ front-web/src/core/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const Navbar = () => {
+    return (
+        <nav className="main-nav">
+            <div className="nav-logo-text">
+                <h4>MovieFlix</h4>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
