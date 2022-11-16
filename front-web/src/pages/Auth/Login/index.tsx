beb8b768528816bb4c53f5576c1fa7c72f74e8d0
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-11-16 14:07:13 -0300

    Adding React Hook Form in login form

--- front-web/src/pages/Auth/Login/index.tsx
+++ front-web/src/pages/Auth/Login/index.tsx
@@ -1,33 +1,65 @@
 
 import React from 'react';
 import Button from '../../../components/Button';
+import { useForm } from 'react-hook-form';
 import './styles.scss';
+
+type FormState = {
+    username: string;
+    password: string;
+}
 
 const Login = () => {
 
-    return (
-        <form className='login-form'>
-            <div className="login-input-email">
-                <input
-                    type="email"
-                    className={`form-control input-base`}
-                    placeholder="Email"
-                    name="username"
-                />
-            </div>
-            <div>
-                <input
-                    type="password"
-                    className={`form-control input-base`}
-                    placeholder="Senha"
-                    name="password"
-                />
-            </div>
-            <div className="login-submit">
-                <Button text='Fazer login' />
-            </div>
+    const { register, handleSubmit, formState: { errors } } = useForm<FormState>();
 
-        </form>
+    const onSubmit = (data: FormState) => {
+        console.log(data);
+    }
+
+    return (
+        <div>
+            <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
+                <div className="login-input-email">
+                    <input
+                        type="email"
+                        className={`form-control input-base ${errors.username ? 'is-invalid' : ''}`}
+                        placeholder="Email"
+                        {...register("username", {
+                            required: { value: true, message: "Campo obrigatório!" },
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Email inválido"
+                            }
+                        })}
+                    />
+                    {errors.username && (
+                        <div className="invalid-feedback d-block">
+                            {errors.username.message}
+                        </div>
+                    )}
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        className={`form-control input-base ${errors.password ? 'is-invalid' : ''}`}
+                        placeholder="Senha"
+                        {...register("password", {
+                            required: { value: true, message: "Campo obrigatório!" }
+                        })}
+                    />
+                    {errors.password && (
+                        <div className="invalid-feedback d-block">
+                            {errors.password.message}
+                        </div>
+                    )}
+                </div>
+                <div className="login-submit">
+                    <Button text='Fazer login' />
+                </div>
+
+            </form>
+        </div>
     )
 };
 
