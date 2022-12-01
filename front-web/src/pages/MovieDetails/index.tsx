41d646b327b26435bd44494c0bcc1d26750f7c9f
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-12-01 18:09:02 -0300

    Movie details layout

--- front-web/src/pages/MovieDetails/index.tsx
+++ front-web/src/pages/MovieDetails/index.tsx
@@ -8,6 +8,8 @@ import { hasAnyRole } from 'util/auth';
 import { Review } from 'types/review';
 import './styles.css';
 
+import MovieImg from 'assets/images/exemple.png';
+
 type UrlParams = {
     movieId: string;
 }
@@ -40,8 +42,28 @@ function MovieDetails() {
 
     return (
         <div className="container movie-details-container">
-            <div className='moview-details-title'>
-                <h1>Tela detalhes do filme id: {`${movieId}`}</h1>
+
+            <div className="card-base movie-details-card">
+                <div className="movie-details-img">
+                    <img src={MovieImg} alt="Nome do filme" />
+                </div>
+                <div className="movie-details-info">
+                    <h3>O Retorno do Rei</h3>
+                    <span>2013</span>
+                    <p>O olho do inimigo está se movendo.</p>
+                    <div className="movie-details-description">
+                        <p>
+                            O confronto final entre as forças do bem e do mal que lutam pelo
+                            controle do futuro da Terra Média se aproxima. Sauron planeja um
+                            grande ataque a Minas Tirith, capital de Gondor, o que faz com que
+                            Gandalf e Pippin partam para o local na intenção de ajudar a
+                            resistência. Um exército é reunido por Theoden em Rohan, em mais
+                            uma tentativa de deter as forças de Sauron. Enquanto isso, Frodo,
+                            Sam e Gollum seguem sua viagem rumo à Montanha da Perdição para
+                            destruir o anel.
+                        </p>
+                    </div>
+                </div>
             </div>
 
             {hasAnyRole(['ROLE_MEMBER']) &&
